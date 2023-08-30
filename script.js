4f81d8caedc48c7d3b825f45f99db0ba9709d91c
russelljamesmapacpac@gmail.com <russelljamesmapacpac@gmail.com>
2023-08-30 22:38:23 +0800

    update with js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Select the form, button, and textarea elements
+  const form = document.querySelector('.main');
+  const button = document.getElementById('submitButton');
+  const textarea = document.getElementById('myTextarea');
+  const output = document.getElementById('output');
 
+  // Attach a submit event listener to the form
+  form.addEventListener('submit', function(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    // Get the value of the textarea
+    const inputValue = textarea.value;
+    
+    // Display the value in the output element
+    output.textContent = inputValue;
+  });
+});
